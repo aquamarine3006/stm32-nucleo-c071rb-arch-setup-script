d4f558f6cd1661df0e2fa0013b6592587eeab40d
aqua <pavkar02@gmail.com>
2026-05-10 15:17:09 +0530

    updated debug.gdb

---
+++ build/SOURCE/main.d
@@ -0,0 +1,2 @@
+/home/aqua/STM32-NUCLEO-CO71RB/stm32-nucleo-c071rb-arch-setup-script/build/SOURCE/main.o: \
+ /home/aqua/STM32-NUCLEO-CO71RB/stm32-nucleo-c071rb-arch-setup-script/SOURCE/main.c
--- setup.sh
+++ setup.sh
@@ -509,6 +509,13 @@ load
 break main
 
 continue
+
+dashboard -layout assembly source registers variables !memory !breakpoints !expressions !history !stack !threads
+dashboard source -style height 11
+dashboard assembly -style height 10
+dashboard -style syntax_highlighting 'dracula'
+dashboard register -style column-major True
+dashboard assembly -style opcodes True
 EOF
 
 # --------------------------------------------------------------------------
